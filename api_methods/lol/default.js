5d504589e4c9f361b40c9db504ce7173c5b65d91
Vincent URRUTY <92440726+vince1699@users.noreply.github.com>
2022-04-10 18:42:46 +0200

    Update default.js

--- api_methods/lol/default.js
+++ api_methods/lol/default.js
@@ -33,19 +33,52 @@ module.exports = {
             while(champ[j].key != r) {
                 j++;
             }
-            champions_list.push([champ[j].id, champ[j].icon]);
+            champions_list.push([champ[j].name, champ[j].title]);
         }
 
         var embedMsg = new MessageEmbed();
 
         embedMsg
             .setColor('GREEN')
-            .setDescription("List of free champions")
+            .setDescription("List of free champions for this week !")
         
         for (let i in champions_list) {
             embedMsg.addField(champions_list[i][0], champions_list[i][1]);
         }
 
         return embedMsg;
+    },
+
+    mastery: async function(username) {
+        const summonerInfo = await lol.getSummonerByName(username);
+        var summonerMastery = await lol.getChampionMasteriesBySummoner(summonerInfo.id);
+        summonerMastery = (summonerMastery.slice(0,5));
+        var mast = [];
+        var name = [];
+        var image = [];
+        for (j in summonerMastery){
+            var i=0;
+            while(champ[i].key != summonerMastery[j].championId) {
+                i++;
+            }
+            name[j] = champ[i].name;
+            image[j] = champ[i].icon;
+            mast[j] = summonerMastery[j].championPoints;
+            console.log(image[0])
+        }
+        
+        var embedMsg = new MessageEmbed();
+
+        embedMsg
+            .setColor('BLUE')
+            .setImage(image[0])
+            .setDescription("Number of mastery points of "+ summonerInfo.name + " 5 most played champions ! ")
+        
+        for (let i = 0; i<=4; i++) {
+            embedMsg.addField(name[i], mast[i].toString());
+        }
+        console.log(summonerInfo);
+        return embedMsg;
+        
     }
 }
